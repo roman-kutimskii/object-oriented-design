9683b0181a3504cdb4292d07243d6ffe1b04aed8
Roman Kutimskii <roman.kutimskii@gmail.com>
2024-11-06 12:22:31 +0300

    Добавить IImage; добавить MockImage

---
+++ command/tests/MockImage.h
@@ -0,0 +1,19 @@
+#ifndef MOCKIMAGE_H
+#define MOCKIMAGE_H
+
+#include <gmock/gmock.h>
+
+#include "../src/IImage.h"
+
+class MockImage final : public IImage
+{
+public:
+    MOCK_METHOD(const std::string &, GetPath, (), (const, override));
+    MOCK_METHOD(int, GetWidth, (), (const, override));
+    MOCK_METHOD(int, GetHeight, (), (const, override));
+    MOCK_METHOD(bool, GetDeleted, (), (const, override));
+    MOCK_METHOD(void, Resize, (int width, int height), (override));
+    MOCK_METHOD(void, SetDeleted, (bool value), (override));
+};
+
+#endif // MOCKIMAGE_H
--- command/tests/main.cpp
+++
@@ -1,3 +0,0 @@
-//
-// Created by roman on 10/30/2024.
-//
