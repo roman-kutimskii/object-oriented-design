e18b91fa26581094921d85a222d39550bbe5b566
Roman Kutimskii <roman.kutimskii@gmail.com>
2024-11-06 13:24:44 +0300

    Добавить IDocument; добавить MockDocument

---
+++ command/src/IDocument.h
@@ -0,0 +1,25 @@
+#ifndef IDOCUMENT_H
+#define IDOCUMENT_H
+
+#include <optional>
+#include <string>
+
+#include "DocumentItem.h"
+
+class IDocument
+{
+public:
+    virtual ~IDocument() = default;
+    virtual void InsertParagraph(const std::string &text, std::optional<size_t> position) = 0;
+    virtual void InsertImage(const std::string &path, int width, int height, std::optional<size_t> position) = 0;
+    [[nodiscard]] virtual size_t GetItemsCount() const = 0;
+    virtual DocumentItem *GetItem(size_t position) = 0;
+    [[nodiscard]] virtual const DocumentItem *GetItem(size_t position) const = 0;
+    virtual void DeleteItem(size_t position) = 0;
+    [[nodiscard]] virtual const std::string &GetTitle() const = 0;
+    virtual void SetTitle(const std::string &title) = 0;
+    virtual void Save(const std::string &path) const = 0;
+    virtual void SetDeleted(size_t position, bool value) = 0;
+};
+
+#endif // IDOCUMENT_H
---
+++ command/tests/MockDocument.h
@@ -0,0 +1,25 @@
+#ifndef MOCKDOCUMENT_H
+#define MOCKDOCUMENT_H
+
+#include <gmock/gmock.h>
+
+#include "../src/IDocument.h"
+
+class MockDocument : public IDocument
+{
+public:
+    MOCK_METHOD(void, InsertParagraph, (const std::string &text, std::optional<size_t> position), (override));
+    MOCK_METHOD(
+        void, InsertImage, (const std::string &path, int width, int height, std::optional<size_t> position), (override)
+    );
+    MOCK_METHOD(size_t, GetItemsCount, (), (const, override));
+    MOCK_METHOD(DocumentItem *, GetItem, (size_t position), (override));
+    MOCK_METHOD(const DocumentItem *, GetItem, (size_t position), (const, override));
+    MOCK_METHOD(void, DeleteItem, (size_t position), (override));
+    MOCK_METHOD(const std::string &, GetTitle, (), (const, override));
+    MOCK_METHOD(void, SetTitle, (const std::string &title), (override));
+    MOCK_METHOD(void, Save, (const std::string &path), (const, override));
+    MOCK_METHOD(void, SetDeleted, (size_t position, bool value), (override));
+};
+
+#endif // MOCKDOCUMENT_H
